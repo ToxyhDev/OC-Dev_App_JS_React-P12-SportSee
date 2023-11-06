d9bdc88265b8377c5e6976a1c0c6b9c29b39e33e
Tom Andre <tom.andre.dev@outlook.com>
2023-11-06 11:51:48 +0100

    Install configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from '@svgr/rollup'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '',
+  plugins: [react(), svgr()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import './src/main.scss';`,
+      },
+    },
+  },
 })
