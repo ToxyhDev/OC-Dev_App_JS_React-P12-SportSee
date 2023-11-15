5bf2de42d605c9abb6329ca4579e5ee84f2f93d9
Tom Andre <tom.andre.dev@outlook.com>
2023-11-15 15:39:48 +0100

    fix: changing basename for publish app on github page

--- src/main.tsx
+++ src/main.tsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter(
     },
   ],
   {
-    basename: '/',
+    basename: '/projets/12-sportsee',
   }
 )
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/projets/12-sportsee/',
   plugins: [react(), svgr()],
   css: {
     preprocessorOptions: {
